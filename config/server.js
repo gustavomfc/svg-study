d5e00188799f5808ae8c86697a22df5929087cbb
gustavomfc <gustavo.mfc@icloud.com>
2020-01-20 06:49:10 +0000

    We'll rock you now with live Server Side EvEvents

--- config/server.js
+++ config/server.js
@@ -1,8 +1,14 @@
 var express=require('express');
 var app=express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', './lib/views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({
+    strict: false
+  }))
 
 //HTTP on port 3000
 app.listen(3000);
